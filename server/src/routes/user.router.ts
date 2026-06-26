3d10135bbbd63c3c057905d3c8df09356603f2f6
Priyanshu <priyanshukushwah0331@gmail.com>
2026-06-26 14:02:33 +0530

    Email verification done

--- server/src/routes/user.router.ts
+++ server/src/routes/user.router.ts
@@ -6,6 +6,6 @@ const router=express.Router()
 
 router.post("/signup",userController.signup);
 router.post("/signin",userController.signin)
-
+router.get("/verify-email/:token",userController.verifyEmail)
 
 export default router
